3df120010f0883e1d9b0d8fa9a3ae63af95b9c1d
Alilouch Reda <alilouchreda328black@gmail.com>
2024-10-31 22:58:36 +0100

    api

---
+++ api/api1.js
@@ -0,0 +1,19 @@
+var client_id = '8488770d6a074502b2eca0b8cfecdbb0';
+var client_secret = '574ce51d4be2452f86cafbfa628096b0';
+
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    var token = body.access_token;
+  }
+});
